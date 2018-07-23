f4bad14621b95d297c4dcc5261ada1c046c2cc16
Omar Finol-Evans <ofinolevans@omar-work.dhcp.lexmark.com>
2018-07-22 21:44:04 -0400

    Initial Commit

---
+++ Myriad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Myriad
+//
+//  Created by Omar Finol-Evans on 7/22/18.
+//  Copyright © 2018 Omar Finol-Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
